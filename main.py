b29c4acdc0aa35ebb5b3206626da6f160ccdfa68
SkyZH <iSkyZH@gmail.com>
2016-01-30 22:42:45 +0800

    update config and main

--- main.py
+++ main.py
@@ -1,16 +1,82 @@
 #!/usr/bin/env python3
 
+import asyncio
+import functools
+import random
+import logging
+
 import ddcm
 import utils
 import wordcount
 
-@utils.Service
+async def start(loop, config, service):
+    if "entrance" in config:
+        future = await service.tcpService.call.ping(ddcm.Remote(
+            host = config["entrance"]["host"],
+            port = config["entrance"]["port"]
+        ))
+        result = await future
+        """service.route.addNode(ddcm.Node(
+            ddcm.utils.dump_node_hex(config["entrance"]["id"]),
+            ddcm.Remote(
+                host = config["entrance"]["host"],
+                port = config["entrance"]["port"]
+            )
+        ))"""
+    if "commit" in config:
+        await service.commit({
+            "to": [config["commit"]["url"]],
+            "done": [],
+            "words": {}
+        })
+
+def generate_url(size):
+    for i in range(size):
+        n = random.randrange(100)
+        yield "https://skyzh.github.io/social-network-site/" + str(n) + ".html"
+
+@utils.SpecifiedService("B")
 async def main(loop, config, service):
-    fetcher = wordcount.Fetcher("https://skyzh.github.io/social-network-site/0.html")
-    data = fetcher.fetch()
-    reader = wordcount.Reader(data)
-    parser = reader.parse()
-    stat = wordcount.Stat(parser)
+    logger = service.logger.get_logger("Main")
+    logger.info("Preparing...")
+    await start(loop, config, service)
+    logger.info("Running...")
+    try:
+        while True:
+            while True:
+                commit_id, _data = await service.get_latest_commit()
+                if _data != None:
+                    break
+            logger.info("Get Commit %(id)s" % {
+                "id": utils.get_hash_string(commit_id)
+            })
+            data = _data["data"]
+            if len(data["to"]) == 0:
+                logger.info("Task End")
+                break
+            url_id = random.randrange(len(data["to"]))
+            url = data["to"][url_id]
+            logger.info("Fetching %(url)s" % {
+                "url": url
+            })
+            fetcher = wordcount.Fetcher(url)
+            response = fetcher.fetch()
+
+            logger.info("Stat...")
+            reader = wordcount.Reader(response)
+            parser = reader.parse()
+            stat = wordcount.Stat(parser)
+
+            del data["to"][url_id]
+            data["done"].append(url)
+            data["to"].extend([i for i in generate_url(5) if not(i in data["done"])])
+            data["words"] = stat.stat()
+
+            logger.info("Committing...")
+            await service.commit(data)
+
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-import ddcm
 import asyncio
+import codecs
+
+import ddcm
 
 def Service(func):
     def _deco(*args, **kwargs):
@@ -22,3 +24,30 @@ def Service(func):
         loop.run_until_complete(service.stop())
         return ret
     return _deco
+
+def SpecifiedService(name):
+    def __deco(func):
+        def _deco(*args, **kwargs):
+            config = ddcm.utils.load_config("config/config" + name + ".json")
+
+            loop = asyncio.get_event_loop()
+            loop.set_debug(config['debug']['asyncio']['enabled'])
+
+            service = ddcm.Service(config, loop)
+            loop.run_until_complete(service.start())
+
+            kwargs = {
+                'loop': loop,
+                'config': config,
+                'service': service
+            }
+
+            ret = loop.run_until_complete(func(*args, **kwargs))
+
+            loop.run_until_complete(service.stop())
+            return ret
+        return _deco
+    return __deco
+
+def get_hash_string(bytes_seq):
+    return codecs.encode(bytes_seq, "hex").decode()
