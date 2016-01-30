7a5fb0fe2f7877fdbdcfa31b1b5e5862be6da431
SkyZH <iSkyZH@gmail.com>
2016-01-30 13:19:41 +0800

    add reducer url test

--- wordcount/reducer.py
+++ wordcount/reducer.py
@@ -1,17 +1,20 @@
 class Reducer(object):
     def __init__(self):
         pass
-    def do_reduce(dataA, dataB):
+    def word_counter(self, word_list, origin_list):
+        for word, count in origin_list.items():
+            if word in word_list:
+                word_list[word] += count
+            else:
+                word_list[word] = count
+    def do_reduce(self, dataA, dataB):
         data = {}
         data["done"] = list(set(dataA["done"] + dataB["done"]))
         data["to"] = [
             i for i in set(dataA["to"] + dataB["to"]) if not(i in data["done"])
         ]
-        data["words"] = {}
-        with data["words"] as word_list:
-            for word, count in dataA["words"].items() + dataB["words"].items():
-                if word in word_list:
-                    word_list[word] += count
-                else:
-                    word_list[word] = count
+        word_list = {}
+        self.word_counter(word_list, dataA["words"])
+        self.word_counter(word_list, dataB["words"])
+        data["words"] = word_list
         return data
--- wordcount/test/reducer_test.py
+++ wordcount/test/reducer_test.py
@@ -5,5 +5,107 @@ import wordcount
 from . import const
 
 class ReducerTest(unittest.TestCase):
-    def test_reducer(self):
-        pass
+    def test_reducer_url_done(self):
+        reducer = wordcount.Reducer()
+        dataA = {
+            "done": [
+                "https://skyzh.github.io/social-network-site/1.html",
+                "https://skyzh.github.io/social-network-site/2.html",
+                "https://skyzh.github.io/social-network-site/3.html",
+                "https://skyzh.github.io/social-network-site/4.html"
+            ],
+            "to": [],
+            "words": {
+            }
+        }
+        dataB = {
+            "done": [
+                "https://skyzh.github.io/social-network-site/5.html",
+                "https://skyzh.github.io/social-network-site/6.html",
+                "https://skyzh.github.io/social-network-site/7.html"
+            ],
+            "to": [],
+            "words": {
+            }
+        }
+        data = reducer.do_reduce(dataA, dataB)
+        self.assertEqual(sorted(data["done"]), sorted([
+                "https://skyzh.github.io/social-network-site/1.html",
+                "https://skyzh.github.io/social-network-site/2.html",
+                "https://skyzh.github.io/social-network-site/3.html",
+                "https://skyzh.github.io/social-network-site/4.html",                "https://skyzh.github.io/social-network-site/5.html",
+                "https://skyzh.github.io/social-network-site/6.html",
+                "https://skyzh.github.io/social-network-site/7.html"
+        ]))
+    def test_reducer_url_to(self):
+        reducer = wordcount.Reducer()
+        dataA = {
+            "to": [
+                "https://skyzh.github.io/social-network-site/1.html",
+                "https://skyzh.github.io/social-network-site/2.html",
+                "https://skyzh.github.io/social-network-site/3.html",
+                "https://skyzh.github.io/social-network-site/4.html"
+            ],
+            "done": [],
+            "words": {
+            }
+        }
+        dataB = {
+            "to": [
+                "https://skyzh.github.io/social-network-site/5.html",
+                "https://skyzh.github.io/social-network-site/6.html",
+                "https://skyzh.github.io/social-network-site/7.html"
+            ],
+            "done": [],
+            "words": {
+            }
+        }
+        data = reducer.do_reduce(dataA, dataB)
+        self.assertEqual(sorted(data["to"]), sorted([
+                "https://skyzh.github.io/social-network-site/1.html",
+                "https://skyzh.github.io/social-network-site/2.html",
+                "https://skyzh.github.io/social-network-site/3.html",
+                "https://skyzh.github.io/social-network-site/4.html",                "https://skyzh.github.io/social-network-site/5.html",
+                "https://skyzh.github.io/social-network-site/6.html",
+                "https://skyzh.github.io/social-network-site/7.html"
+        ]))
+    def test_reducer_url_to_except(self):
+        reducer = wordcount.Reducer()
+        dataA = {
+            "to": [
+                "https://skyzh.github.io/social-network-site/1.html",
+                "https://skyzh.github.io/social-network-site/2.html",
+                "https://skyzh.github.io/social-network-site/3.html",
+                "https://skyzh.github.io/social-network-site/4.html"
+            ],
+            "done": [
+                "https://skyzh.github.io/social-network-site/1.html",
+                "https://skyzh.github.io/social-network-site/5.html"
+            ],
+            "words": {
+            }
+        }
+        dataB = {
+            "to": [
+                "https://skyzh.github.io/social-network-site/5.html",
+                "https://skyzh.github.io/social-network-site/6.html",
+                "https://skyzh.github.io/social-network-site/7.html"
+            ],
+            "done": [
+                "https://skyzh.github.io/social-network-site/2.html",
+                "https://skyzh.github.io/social-network-site/6.html"
+            ],
+            "words": {
+            }
+        }
+        data = reducer.do_reduce(dataA, dataB)
+        self.assertEqual(sorted(data["to"]), sorted([
+                "https://skyzh.github.io/social-network-site/3.html",
+                "https://skyzh.github.io/social-network-site/4.html",                                              "https://skyzh.github.io/social-network-site/7.html"
+        ]))
+        self.assertEqual(sorted(data["done"]), sorted([
+                "https://skyzh.github.io/social-network-site/1.html",
+                "https://skyzh.github.io/social-network-site/5.html",
+                "https://skyzh.github.io/social-network-site/2.html",
+                "https://skyzh.github.io/social-network-site/6.html"
+        ]))
